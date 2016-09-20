8f89caee8e701e56172de21f6d1e45c019ddb61f
ZhouYzzz <zhouyz9608@gmail.com>
2016-09-20 09:55:14 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Yizhuang Zhou on 9/20/16.
+//  Copyright © 2016 Yizhuang Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
